4c473725f4772e6f4652603b6feba7b4e2608211
Fitri Farida Solihati <fitri02farida@gmail.com>
2024-04-28 02:01:37 +0700

    Fitri (28 April 2024, 02:01)

--- Assets/Script/StartScreen.cs
+++ Assets/Script/StartScreen.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public GameObject LoadingScreen;
+    public Slider progressSlider;
     public GameObject Option;
 
         //START MENU//
-    public void StartGame(){
-        SceneManager.LoadScene("InGame");
+    public void StartGame(int index)
+    {
+        StartCoroutine(LoadScene_Coroutine(index));
+        // SceneManager.LoadScene("InGame");
         AudioManager.Instance.musicSource.Stop();
         AudioManager.Instance.PlaySFX("Button");
     }
@@ -38,6 +43,26 @@ public class NewBehaviourScript : MonoBehaviour
         AudioManager.Instance.PlaySFX("Button");
     }
 
+    public IEnumerator LoadScene_Coroutine(int index)
+    {
+        progressSlider.value = 0;
+        LoadingScreen.SetActive(true);
+
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(index);
+        asyncOperation.allowSceneActivation = false;
+        float progress = 0;
+        while (!asyncOperation.isDone)
+        {
+            progress = Mathf.MoveTowards(progress, asyncOperation.progress, Time.deltaTime);
+            progressSlider.value = progress;
+            if (progress >=0.9f)
+            {
+                progressSlider.value = 1;
+                asyncOperation.allowSceneActivation = true;
+            }
+            yield return null;
+        }
+    }
     
     //LOAD MENU//
     public void StartMenu(){
